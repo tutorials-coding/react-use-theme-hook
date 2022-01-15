78fb035f855f38bd5041cf70639beb0252ef41c0
Vadim <korobeinikovv@mail.ru>
2022-01-15 13:07:25 +0300

    Add local storage theme persistence

--- src/hooks/use-theme.js
+++ src/hooks/use-theme.js
@@ -1,10 +1,13 @@
 import { useLayoutEffect, useState } from 'react'
 
 export const useTheme = () => {
-  const [theme, setTheme] = useState('light')
+  const [theme, setTheme] = useState(
+    localStorage.getItem('app-theme') || 'light'
+  )
 
   useLayoutEffect(() => {
     document.documentElement.setAttribute('data-theme', theme)
+    localStorage.setItem('app-theme', theme)
   }, [theme])
 
   return { theme, setTheme }
