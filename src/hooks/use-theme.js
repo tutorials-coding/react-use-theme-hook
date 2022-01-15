c44d47648e2526bf59dad6ff916e0d694f7286f3
Vadim <korobeinikovv@mail.ru>
2022-01-15 13:17:29 +0300

    Default value depends on system theme

--- src/hooks/use-theme.js
+++ src/hooks/use-theme.js
@@ -1,5 +1,9 @@
 import { useLayoutEffect, useState } from 'react'
 
+const isDarkTheme = window?.matchMedia('(prefers-color-scheme: dark)').matches
+const defaultTheme = isDarkTheme ? 'dark' : 'light'
+console.log('defaultTheme', defaultTheme)
+
 export const useTheme = () => {
   const [theme, setTheme] = useState(
     localStorage.getItem('app-theme') || 'light'
