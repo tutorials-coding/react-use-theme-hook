34ff285d9b01be1fd8d6f1df0da134e1c727189b
Vadim <korobeinikovv@mail.ru>
2022-01-15 13:04:41 +0300

    Add click handlers

--- src/App.js
+++ src/App.js
@@ -11,12 +11,23 @@ import './App.css'
 export default function App() {
   const { theme, setTheme } = useTheme()
 
+  const handleLightThemeClick = () => {
+    setTheme('light')
+  }
+  const handleDarkThemeClick = () => {
+    setTheme('dark')
+  }
+
   return (
     <div className="app__container w-100 h-100 d-flex flex-column">
       <div className="p-3 d-flex justify-content-end">
         <ButtonGroup aria-label="Theme toggle">
-          <Button variant="secondary">Light</Button>
-          <Button variant="secondary">Dark</Button>
+          <Button variant="secondary" onClick={handleLightThemeClick}>
+            Light
+          </Button>
+          <Button variant="secondary" onClick={handleDarkThemeClick}>
+            Dark
+          </Button>
         </ButtonGroup>
       </div>
       <div className="flex-grow-1 d-flex flex-column justify-content-center align-items-center">
